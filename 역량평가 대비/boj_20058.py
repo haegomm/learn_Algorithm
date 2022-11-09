6b3b49cd80843e772ce0dae839028cf7c45a3f7b
haegomm <rkdms4054@gmail.com>
2022-11-10 00:13:19 +0900

    update

--- 역량평가 대비/boj_20058.py
+++ 역량평가 대비/boj_20058.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 # 얼음 회전
 def turn(s):
     print(1)
@@ -26,73 +25,38 @@ dy = [0, 0, -1, 1]
 
 # 인접 얼음들 확인
 def checkice(firestorm):
-    global arr
-    print(2)
+
     zero_friends = []
 
     # 0인 얼음을 먼저 찾기
     for i in range(k):
         for j in range(k):
             if not firestorm[i][j]:
-=======
-# 배열을 단계별로 회전시키기
-def trun(s):
-    new = [[0]*k for _ in range(k)]
-
-    for i in range(0, k - s + 1, s):
-        for j in range(0, k - s + 1, s):
-            new[j][k-s-i] = arr[i][j]
-
-    check_ice(new)
-
-# 얼음 확인
-dx = [-1, 1, 0, 0]
-dy = [0, 0, -1, 1]
-
-def check_ice(new):
-
-    check = []
-
-    # 0인 얼음 찾아서 확인할 얼음 리스트 만들기
-    for i in range(k):
-        for j in range(k):
-            if new[i][j] == 0:
->>>>>>> 654183155016e75adb3533efd921f7300ae9b836
                 for d in range(4):
                     nx = i + dx[d]
                     ny = j + dy[d]
                     if 0 <= nx < k and 0 <= ny < k:
-<<<<<<< HEAD
                         zero_friends.append((nx, ny))
 
-    for x, y in zero_friends:
-=======
-                        check.append((nx, ny))
+    zero_friends.clear()
 
-    # 확인할 얼음의 인접 얼음들 확인
-    for x, y in check:
->>>>>>> 654183155016e75adb3533efd921f7300ae9b836
+    for x, y in zero_friends:
         cnt = 0
         for d in range(4):
             nx = x + dx[d]
             ny = y + dy[d]
-<<<<<<< HEAD
             if 0 <= nx < k and 0 <= ny < k and not firestorm[nx][ny]:
-                # 0인 얼음이 있으면 +
                 cnt += 1
-                # 0인 얼음이 주변에 1개보다 많으면 -
                 if cnt > 1:
                     firestorm[x][y] -= 1
-                    break
+                    
+    # 꼭짓점 모서리 얼음 녹이기
+    # 배열 바꾸기
 
-    zero_friends.clear()
 
-
-# 가장 큰 얼음덩어리 찾기
 def dfs(x, y):
     global ice_size
 
-    print(3)
     visited[x][y] = True
     ice_size += 1
 
@@ -101,64 +65,30 @@ def dfs(x, y):
         ny = y + dy[d]
         if 0 <= nx < k and 0 <= ny < k and not visited[nx][ny] and not arr[nx][ny]:
             dfs(nx, ny)
-=======
-            if 0 <= nx < k and 0 <= ny < k and new[nx][ny] == 0:
-                cnt += 1
-                if cnt > 1:
-                    new[x][y] -= 1
 
-    check.clear()
-
-    # 남아있는 얼음의 합
-    ice_sum = 0
-
-    for line in new:
-        ice_sum += sum(line)
-
-    bigice(new)
-
-
-def bigice():
-
-    visited = [[False] * k for _ in range(k)]
-
-    ice_size = []
-
-    for i in range(k):
-        for j in range(k):
-            if not visited[i][j]:
-
-
->>>>>>> 654183155016e75adb3533efd921f7300ae9b836
+    big_ice.append(ice_size)
 
 
 n, q = map(int, input().split())
 k = 2**n
 arr = [list(map(int, input().split())) for _ in range(k)]
-stages = list(map(int, input().split()))
-<<<<<<< HEAD
+stages = [list(map(int, input().split()))]
 visited = [[False] * k for _ in range(k)]
 ice_sum = 0
 big_ice = 0
 
 for stage in stages:
-    turn(2 ** stage)
+    turn(2**stage)
 
 for line in arr:
     ice_sum += sum(line)
 
 for i in range(k):
     for j in range(k):
-        if not visited[i][j] and arr[i][j]:
+        if not visited[i][j] and not arr[i][j]:
             ice_size = 0
-            dfs(i, j)
+            dfs(i,j)
             if big_ice < ice_size:
                 big_ice = ice_size
 
 print(ice_sum, big_ice, sep='\n')
-
-=======
-
-for stage in stages:
-    trun(stage)
->>>>>>> 654183155016e75adb3533efd921f7300ae9b836
--- 역량평가 대비/prog_12930_이상한 문자만들기.py
+++ 역량평가 대비/prog_12930_이상한 문자만들기.py
@@ -3,13 +3,6 @@ def solution(s):
     answer = []
 
     for word in words:
-<<<<<<< HEAD
-        word = list(word)
-        for i in range(0, len(word), 2):
-            word[i] = word[i].upper()
-        word = "".join(word)
-        answer += [word]
-=======
         new_word = ''
         for i, values in enumerate(word):
             if i % 2 == 0:
@@ -17,7 +10,6 @@ def solution(s):
             else:
                 new_word += values.lower()
         answer += [new_word]
->>>>>>> 654183155016e75adb3533efd921f7300ae9b836
     answer = " ".join(answer)
 
     return answer
